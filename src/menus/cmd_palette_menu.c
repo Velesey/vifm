fc518560eb743bb4a20d5192e74f77dd5b698057
Vladimir Baranov <velesey@gmail.com>
2026-05-20 08:57:20 +0500

    cr

--- src/menus/cmd_palette_menu.c
+++ src/menus/cmd_palette_menu.c
@@ -1,6 +1,4 @@
 /* vifm
- * Copyright (C) 2001 Ken Steen.
- * Copyright (C) 2011 xaizek.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
